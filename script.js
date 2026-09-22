721116c2abab7c5caa0f9cc55730ac2b39f0593a
Surendra906 <surendramode945@gmail.com>
2026-09-22 13:09:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,43 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function toggleplay(){
+	if(video.paused){
+		video.play();
+	}else{
+		video.pause();
+	}
+}
+
+function updateButton() {
+	const icon=this.paused?'►':'❚ ❚';
+	toggle.textContent=icon;
+}
+
+function skip(){
+	video.currentTime+=parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+	const percent=(video.currentTime/video.duration)*100;
+	progressBar.style.flexBasis=`${percent}%`;
+}
+function scrub(e){
+	const scrubTime=(e.offStX/progress.offsetWidth)*video.duration;
+	video.currentTime=scrubTime;
+}
+
+video.addEventListener('click',toggleplay);
+video.addEventListener('play',updateButton);
+video.addEventListener('pause',updateButton);
+video.addEventListener('timeupdate',handleProgress);
+toggle.addEventListener('click',toggleplay);
+skipButtons.forEach(button=>button.addEventlistener('click',skip));
+ranges.forEach(range=>range.addEventListener('change',handleRangeUpdate));
+range.forEach(range=>range.addEventListener('mousemove',handleRangeUpdate));
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
